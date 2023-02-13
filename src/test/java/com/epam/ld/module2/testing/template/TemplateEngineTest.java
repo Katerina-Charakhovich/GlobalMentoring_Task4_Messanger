2d04b68ef43f5fa747e1f93484b61728441a781a
Katerina_Charakhovich <v-kcharakhovich@expediagroup.com>
2023-02-13 13:41:26 +0100

    TDD_approach: add conditions 3,4

--- src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
+++ src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
@@ -7,42 +7,40 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 //1.The system replaces variable placeholders like #{subject} from a template with values provided at runtime.
 //2.If at least one placeholder value is not provided at runtime – template generator should throw an exception.
 //3.Template generator ignores values for variables provided at runtime that aren’t found from the template.
-//4.System should support values passed in runtime with #{…}. E.g. template is  “Some text: #{value}” and  at runtime #{value} passed as  #{tag}. Output should be “Some text: #{tag}”.
+//4.System should support values passed in runtime with #{…}. E.g. template is  “Some text: #{value} and  at runtime #{value} passed as  #{tag}. Output should be “Some text: #{tag}
 //5.The system supports the full Latin-1 character set in templates and in variables.
 //6/Messenger should work in two modes: console and file mode.
 //
 //In console mode the application takes expression from console and prints result to console. No application parameters should be specified to use this mode.
 //In file mode application takes expression from file and output results to file. To use this mode user should specify input and output file names as application parameters.
 public class TemplateEngineTest {
-    TemplateEngine templateEngine;
-    Client client;
+    private static TemplateEngine templateEngine = new TemplateEngine();
+    private static Client client = new Client();
 
     @BeforeAll
     public static void init() {
-        TemplateEngine templateEngine = new TemplateEngine();
-        Client client = new Client();
         client.setAddresses("test@test.com");
         HashMap<String, String> tags = new HashMap<>();
         tags.put("value", "test");
-        client.setTags(new HashMap<>());
+        client.setTags(tags);
     }
 
-//   1.The system replaces variable placeholders like #{subject} from a template with values provided
-//   at runtime.
+    //   1.The system replaces variable placeholders like #{subject} from a template with values provided
+    //   at runtime.
     @Test
-    public void shouldReturnTemolate(){
-        assertEquals("Some text: test", templateEngine
-                .generateMessage(new Template("Some text: #{value}"),client));
+    public void shouldReturnTemplate() {
+            assertEquals("Some text: test", templateEngine
+                    .generateMessage(new Template("Some text: #{value}"), client));
     }
 
     //2.If at least one placeholder value is not provided at runtime – template generator should throw
     // an exception.
-
     @Test
     public void shouldThrowIllegalStateException_whenValueIsUnknown() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
@@ -50,4 +48,23 @@ public class TemplateEngineTest {
                     .generateMessage(new Template("Some text: #{exception}"), client);
         });
     }
+
+    //3.Template generator ignores values for variables provided at runtime that aren’t found from
+    // the template.
+    @Test
+    public void shouldReturnTemplate_whenReadUnknownTag() {
+        client.getTags().put("unknown", "test");
+        assertEquals("Some text: test", templateEngine
+                .generateMessage(new Template("Some text: #{value}"), client));
+    }
+
+    //4.System should support values passed in runtime with #{…}. E.g. template is  “Some text: #{value}
+    // and  at runtime #{value} passed as  #{tag}. Output should be “Some text: #{tag}
+    @Test
+    public void shouldReturnTemplate_supportSpecificFormat() {
+        client.getTags().put("sometag", "#{tag}");
+        assertEquals("Some text: #{tag}", templateEngine
+                .generateMessage(new Template("Some text: #{sometag}"), client));
+    }
+
 }

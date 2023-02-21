985b6d4f2c2a5a4bf63e5d9c43bcf6fab7303672
Katerina_Charakhovich <v-kcharakhovich@expediagroup.com>
2023-02-21 14:24:49 +0100

    TDD_approach: 5. Mock reading from file/console

---
+++ src/test/java/com/epam/ld/module2/testing/ConsoleExecutorTest.java
@@ -0,0 +1,30 @@
+package com.epam.ld.module2.testing;
+
+import com.epam.ld.module2.testing.util.ScannerHelper;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class ConsoleExecutorTest {
+
+    @Test
+    void execute_withMockScanner() {
+        HashMap<String, String> tags = new HashMap<>();
+        tags.put("value", "test");
+        Client client = mock (Client.class);
+        ScannerHelper scannerHelper = mock(ScannerHelper.class);
+        ConsoleExecutor consoleExecutor = new ConsoleExecutor(scannerHelper);
+        when(scannerHelper.readTemplateFromConsole()).thenReturn("Some template #{value}");
+        when(scannerHelper.getClient(any())).thenReturn(client);
+        when(scannerHelper.reaTagValue(anyList())).thenReturn(tags);
+        when(client.getTags()).thenReturn(tags);
+        boolean result = consoleExecutor.execute();
+        assertTrue(result);
+    }
+}
---
+++ src/test/java/com/epam/ld/module2/testing/FileExecutorTest.java
@@ -0,0 +1,45 @@
+package com.epam.ld.module2.testing;
+
+import com.epam.ld.module2.testing.template.TemplateEngine;
+import com.epam.ld.module2.testing.util.FileHelper;
+import com.epam.ld.module2.testing.util.ScannerHelper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import org.mockito.Mock;
+import org.mockito.Spy;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class FileExecutorTest {
+
+    @Test
+    public void execute_withMockReadFromFile()  throws IOException {
+        HashMap<String, String> tags = new HashMap<>();
+        tags.put("value", "test");
+        Client client = mock (Client.class);
+        ScannerHelper scannerHelper = mock(ScannerHelper.class);
+        FileHelper fileHelper= mock(FileHelper.class);
+        FileExecutor fileExecutor = new FileExecutor("test","test", scannerHelper,fileHelper);
+        when(scannerHelper.getClient(any())).thenReturn(client);
+        when(scannerHelper.reaTagValue(anyList())).thenReturn(tags);
+        when(client.getTags()).thenReturn(tags);
+        when(fileHelper.isFileOrDirectory(any())).thenReturn(1);
+        when(fileHelper.readTemplateFromFile(any())).thenReturn("Some template #{value}");
+        when(fileHelper.saveContentToFile(any(),any())).thenReturn(true);
+        boolean result = fileExecutor.execute();
+        assertTrue(result);
+    }
+
+
+
+}

d62d657a91e8f129d51d6e1e100578b13fc4b211
Katerina_Charakhovich <v-kcharakhovich@expediagroup.com>
2023-02-21 14:20:47 +0100

    TDD_approach: 4.TemporaryFolder rule

---
+++ src/test/java/com/epam/ld/module2/testing/util/FileHelperTest.java
@@ -0,0 +1,24 @@
+package com.epam.ld.module2.testing.util;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileHelperTest {
+    @TempDir
+    File tempDir;
+
+    @Test
+    public void readTemplateFromFile() throws IOException {
+        final File tempFile = new File(tempDir, "tempFile.txt");
+        FileUtils.writeStringToFile(tempFile, "hello world");
+        FileHelper fileHelper = new FileHelper();
+        String actual = fileHelper.readTemplateFromFile(tempFile.getPath());
+        assertEquals("hello world", actual);
+    }
+}
